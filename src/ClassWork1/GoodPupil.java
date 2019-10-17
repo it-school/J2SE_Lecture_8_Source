75022f93d8349f7e71b903273ed14d6cb065456b
Viktor Speranskyy <it@osvita.od.ua>
2019-10-17 21:08:14 +0300

    v.3

---
+++ src/ClassWork1/GoodPupil.java
@@ -0,0 +1,14 @@
+package ClassWork1;
+
+public class GoodPupil extends Pupil {
+    public GoodPupil(String name, int age) {
+        super(name, age);
+        this.characteristics = "good";
+    }
+
+    @Override
+    String relax() {
+        return super.relax() + this.characteristics;
+    }
+}
+
